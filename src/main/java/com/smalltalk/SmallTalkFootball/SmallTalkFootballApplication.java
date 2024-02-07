dfdeb416219ba2fba21bf9b7359ed5c4c59bc584
Alon <alon.benakot@gmail.com>
2024-02-07 21:30:12 +0200

    more

---
+++ src/main/java/com/smalltalk/SmallTalkFootball/SmallTalkFootballApplication.java
@@ -0,0 +1,13 @@
+package com.smalltalk.SmallTalkFootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallTalkFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallTalkFootballApplication.class, args);
+	}
+
+}
