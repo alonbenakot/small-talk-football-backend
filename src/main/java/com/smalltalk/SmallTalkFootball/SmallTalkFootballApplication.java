fcbdefeb893e08513aee39085b300602ff081997
Alon <alon.benakot@gmail.com>
2025-04-30 08:38:43 +0300

    chore: refactor cors configuration to a separate Configuration class

--- src/main/java/com/smalltalk/SmallTalkFootball/SmallTalkFootballApplication.java
+++ src/main/java/com/smalltalk/SmallTalkFootball/SmallTalkFootballApplication.java
@@ -11,17 +11,6 @@ public class SmallTalkFootballApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmallTalkFootballApplication.class, args);
-
-    }
-
-    @Configuration
-    public class WebConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
-                    .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
-        }
     }
 
 }
--- src/main/java/com/smalltalk/SmallTalkFootball/config/WebConfig.java
+++ src/main/java/com/smalltalk/SmallTalkFootball/config/WebConfig.java
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfig {
-
-    @Configuration
-    public class WebConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
-                    .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
-        }
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
+
 }
