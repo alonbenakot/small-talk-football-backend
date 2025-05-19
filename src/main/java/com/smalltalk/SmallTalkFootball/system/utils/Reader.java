51bd0f41c93032e9a88963adf93436facb6b951d
Alon <alon.benakot@gmail.com>
2025-05-19 21:53:49 +0300

    chore: refactor smallInfoInitiator logic to an abstract class and add article reader

--- src/main/java/com/smalltalk/SmallTalkFootball/system/utils/Reader.java
+++ src/main/java/com/smalltalk/SmallTalkFootball/system/utils/Reader.java
@@ -1,7 +1,45 @@
 package com.smalltalk.SmallTalkFootball.system.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public interface Reader<T> {
-    List<T> read();
+
+public abstract class Reader<T> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public List<T> read() {
+        try (Stream<Path> paths = Files.walk(getBasePath())) {
+
+            return paths.filter(Files::isRegularFile)
+                    .map(mapToObject())
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    private Function<Path, T> mapToObject() {
+        return path -> {
+            try {
+                String json = Files.lines(path).collect(Collectors.joining());
+                return objectMapper.readValue(json, getTypeClass());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    protected abstract Path getBasePath();
+
+    protected abstract Class<T> getTypeClass();
 }
