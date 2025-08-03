1c4b9b189ae6ccfb4f4084b5fa9789fe8b251a90
Alon <alon.benakot@gmail.com>
2025-08-03 15:09:45 +0300

    bugfix: use allowedOrigins only once

--- src/main/java/com/smalltalk/SmallTalkFootball/config/WebConfig.java
+++ src/main/java/com/smalltalk/SmallTalkFootball/config/WebConfig.java
@@ -9,9 +9,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://small-talk-football-frontend.vercel.app")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+                .allowedOrigins(
+                        "https://small-talk-football-frontend.vercel.app",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+}
 
 }
