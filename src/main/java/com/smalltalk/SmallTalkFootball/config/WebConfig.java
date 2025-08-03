4738c0c034dae802c9f72235cbcee2ff74e9bc45
Alon <alon.benakot@gmail.com>
2025-08-03 21:09:26 +0300

    feat: add small-talk-football domain

--- src/main/java/com/smalltalk/SmallTalkFootball/config/WebConfig.java
+++ src/main/java/com/smalltalk/SmallTalkFootball/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://small-talk-football-frontend.vercel.app",
+                        "small-talk-football",
                         "http://localhost:5173"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
