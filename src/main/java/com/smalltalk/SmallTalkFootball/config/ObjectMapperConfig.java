4e033d9b5f64db09486f22e874feb05bd1ca2840
Alon <alon.benakot@gmail.com>
2025-10-20 22:32:03 +0300

    feat: send dates an iso strings

--- src/main/java/com/smalltalk/SmallTalkFootball/config/ObjectMapperConfig.java
+++ src/main/java/com/smalltalk/SmallTalkFootball/config/ObjectMapperConfig.java
@@ -3,6 +3,7 @@ package com.smalltalk.SmallTalkFootball.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ public class ObjectMapperConfig {
         return new ObjectMapper()
                 .setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .findAndRegisterModules();
     }
 
