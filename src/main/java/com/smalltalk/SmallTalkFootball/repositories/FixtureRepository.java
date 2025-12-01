e8d940000d5b691f9e3bea97be117e728ca3fb13
Alon <alon.benakot@gmail.com>
2025-12-01 22:27:29 +0200

    bugfix:remove unused import

--- src/main/java/com/smalltalk/SmallTalkFootball/repositories/FixtureRepository.java
+++ src/main/java/com/smalltalk/SmallTalkFootball/repositories/FixtureRepository.java
@@ -4,7 +4,6 @@ import com.smalltalk.SmallTalkFootball.domain.Fixture;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FixtureRepository extends MongoRepository<Fixture, String> {
